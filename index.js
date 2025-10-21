d45b075f48a6172463ece6bb2d66d42b26319d43
Aditya Sharma <absolutecarnage544@gmail.com>
2025-10-21 22:43:52 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const { fetchInventoryData, processInventoryData } = require('./inventory');
 const app = express()
 const port = 5000
